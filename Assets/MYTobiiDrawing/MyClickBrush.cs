946687133aaef7f61d21e48f5753a5984c2a0f79
Tom Bishop <114227637+TomBishop361@users.noreply.github.com>
2024-04-25 23:15:59 +0100

    Lots of Fixes
    weird main menu stopping I can paint colour function from working is fixed
    2Dphysics game mode FIXED

--- Assets/MYTobiiDrawing/MyClickBrush.cs
+++ Assets/MYTobiiDrawing/MyClickBrush.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 using Tobii.Gaming;
 using UnityEngine.SceneManagement;
 
-public class MyClickBrush : MonoBehaviour {
-	
+public class MyClickBrush : MonoBehaviour
+{
+
 	public GameObject GazePoint;
 	public GameObject brushPrefab;
 	public GameObject FollowObj;
@@ -13,105 +14,117 @@ public class MyClickBrush : MonoBehaviour {
 	Vector2 startPos;
 	Plane objPlane;
 	public Color color;
-	Scene currentScene;
-	//Vector3 prevPos;
-	
 
-    public float multiplier = 0.3f;
+	//Vector3 prevPos;
+
+
+	public float multiplier = 0.3f;
 	private Vector2 _historicPoint;
 	private bool _hasHistoricPoint;
 
-    // Called at the start of the scene
-	void Start(){
-		objPlane = new Plane(Camera.main.transform.forward*-1, this.transform.position);
-        currentScene = SceneManager.GetActiveScene();
-    }
+	// Called at the start of the scene
+	void Start()
+	{
+		objPlane = new Plane(Camera.main.transform.forward * -1, this.transform.position);
 
-	
-    public static float lerp(float startValue, float endValue, float t)
-    {
-        return (startValue + (endValue - startValue) * t);
-    }
+	}
 
-    // Update is called once per frame
-    void Update () {
-		
-		GazePoint gazePoint = TobiiAPI.GetGazePoint();      
-		
-        if (Input.GetKeyDown (KeyCode.Mouse1)) {		
-			
-				//Instanciates the Trail object to the FollowObj So that the trail starts at the eye position each time
-				thisBrush = (GameObject)Instantiate(brushPrefab, FollowObj.transform.position, Quaternion.identity);
-				thisBrush.GetComponent<LineScript>().colourChange(color);
-			
 
-            if (Manager.Instance != null)
+
+	public static float lerp(float startValue, float endValue, float t)
+	{
+		return (startValue + (endValue - startValue) * t);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		GazePoint gazePoint = TobiiAPI.GetGazePoint();
+
+		if (Input.GetKeyDown(KeyCode.Mouse1))
+		{
+
+			//Instanciates the Trail object to the FollowObj So that the trail starts at the eye position each time
+			thisBrush = Instantiate(brushPrefab, FollowObj.transform.position, Quaternion.identity);
+			thisBrush.GetComponent<LineScript>().colourChange(color);
+
+
+			if (Manager.Instance != null)
 			{
 				Manager.Instance.lines.Add(thisBrush);
 			}
-			
-            Ray mRay = Camera.main.ScreenPointToRay (gazePoint.Screen);
+
+			Ray mRay = Camera.main.ScreenPointToRay(gazePoint.Screen);
 
 			float rayDistance;
-            _hasHistoricPoint = false;
-			if (objPlane.Raycast (mRay, out rayDistance)) {
-				startPos = mRay.GetPoint (rayDistance);
-				
+			_hasHistoricPoint = false;
+			if (objPlane.Raycast(mRay, out rayDistance))
+			{
+				startPos = mRay.GetPoint(rayDistance);
+
 			}
-		} else if (Input.GetKey (KeyCode.Mouse1) && gazePoint.IsRecent ()) {
-			Ray mRay = Camera.main.ScreenPointToRay (gazePoint.Screen);
+		}
+		else if (Input.GetKey(KeyCode.Mouse1) && gazePoint.IsRecent())
+		{
+			Ray mRay = Camera.main.ScreenPointToRay(gazePoint.Screen);
 
 			float rayDistance;
-			if (objPlane.Raycast (mRay, out rayDistance)) {
+			if (objPlane.Raycast(mRay, out rayDistance))
+			{
 				//Follow Obj and Brush follows the Eye Position 
-				FollowObj.transform.position = smoothFilter(new Vector2(mRay.GetPoint(rayDistance).x,mRay.GetPoint(rayDistance).y));
-                
-                if (thisBrush != null)
+				FollowObj.transform.position = smoothFilter(new Vector2(mRay.GetPoint(rayDistance).x, mRay.GetPoint(rayDistance).y));
+
+				if (thisBrush != null)
 				{
-					thisBrush.transform.position = smoothFilter(new Vector2(mRay.GetPoint(rayDistance).x, mRay.GetPoint(rayDistance).y));
-                    //thisBrush.GetComponent<LineScript>().newPos = smoothFilter(new Vector2(mRay.GetPoint(rayDistance).x, mRay.GetPoint(rayDistance).y));
-                    //thisBrush.GetComponent<LineScript>().newPos = FollowObj.transform.position;
-                }
+					thisBrush.GetComponent<LineScript>().newPos = FollowObj.transform.position;
 
-            }
+				}
 
-			//prevPos = mRay.GetPoint (rayDistance);
-		} else if (Input.GetKey (KeyCode.Mouse1)) {
-			if (Vector2.Distance (thisBrush.transform.position, startPos) < 0.1)
-            {
-                Destroy (thisBrush);
-                _hasHistoricPoint = false;
-            }
+			}
+
 
 		}
-		if(Input.GetKeyUp(KeyCode.Mouse1))
+		else if (Input.GetKey(KeyCode.Mouse1))
+		{
+			if (Vector2.Distance(thisBrush.transform.position, startPos) < 0.1)
+			{
+				Destroy(thisBrush);
+				_hasHistoricPoint = false;
+			}
+
+		}
+		if (Input.GetKeyUp(KeyCode.Mouse1))
 		{
 
 			thisBrush.GetComponent<LineScript>().lineComplete(); //make singleton?
-        }
-		
-		else {
+		}
+
+		else
+		{
 			//While pen is up (Space is not pressed)
-            Ray mRay = Camera.main.ScreenPointToRay(gazePoint.Screen);
-            float rayDist;
+			Ray mRay = Camera.main.ScreenPointToRay(gazePoint.Screen);
+			float rayDist;
 			//Follow Obj will follow Eye Position at all times
-            if (objPlane.Raycast(mRay, out rayDist))
-            {
-				FollowObj.transform.position = smoothFilter(new Vector2(mRay.GetPoint(rayDist).x, mRay.GetPoint(rayDist).y));    
-				
-            }
-        }
-		
+			if (objPlane.Raycast(mRay, out rayDist))
+			{
+				FollowObj.transform.position = smoothFilter(new Vector2(mRay.GetPoint(rayDist).x, mRay.GetPoint(rayDist).y));
+
+			}
+		}
+
 	}
 
-	private Vector2 smoothFilter(Vector2 point){
-		if (!_hasHistoricPoint) {
+	private Vector2 smoothFilter(Vector2 point)
+	{
+		if (!_hasHistoricPoint)
+		{
 			_historicPoint = point;
 			_hasHistoricPoint = true;
 		}
 
-		var smoothedPoint = new Vector2 (point.x * multiplier + _historicPoint.x * (1.0f - multiplier), 
-			                    point.y * multiplier + _historicPoint.y * (1.0f - multiplier));
+		var smoothedPoint = new Vector2(point.x * multiplier + _historicPoint.x * (1.0f - multiplier),
+								point.y * multiplier + _historicPoint.y * (1.0f - multiplier));
 
 		_historicPoint = smoothedPoint;
 
@@ -120,32 +133,33 @@ public class MyClickBrush : MonoBehaviour {
 
 	public void colourRed()
 	{
-		color = new Color(1,0,0);
+		color = new Color(1, 0, 0);
 	}
 	public void colourGreen()
 	{
-		color = new Color(0.1325341f, 1,0);
+		color = new Color(0.1325341f, 1, 0);
 	}
 	public void colourBlue()
 	{
 		color = new Color(0, 0, 1);
 	}
 
-	public void colourYellow() {
-        color = new Color(1, 0.8889837f, 0);
-    }
+	public void colourYellow()
+	{
+		color = new Color(1, 0.8889837f, 0);
+	}
 
 	public void ColourPink()
 	{
 		Vector3 pink = new Vector3(255, 20, 147).normalized;
-		color = new Color(pink.x,pink.y,pink.z);
+		color = new Color(pink.x, pink.y, pink.z);
 	}
 
 	public void colourBrown()
 	{
 		Vector3 bown = new Vector3(139, 69, 19).normalized;
-        color = new Color(bown.x,bown.y,bown.z);
-    }
+		color = new Color(bown.x, bown.y, bown.z);
+	}
 
 
 
--- Assets/Script/LineScript.cs
+++ Assets/Script/LineScript.cs
@@ -1,26 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+//using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LineScript : MonoBehaviour
-{
+{   
+    Scene currentScene;
     Color color = Color.black;
     LineRenderer lineRenderer;
     [SerializeField] private EdgeCollider2D _collider2D;
     public Rigidbody2D rb;
     bool _enabled = true;
     public Vector3 newPos;
-
+    float radius;
     private List<Vector2> points = new List<Vector2>();
     // Start is called before the first frame update
-
+    
+    bool physicsScene;
     public const float RESOLUTION = 1.5f;
     void Start()
     {
-       
-       lineRenderer = GetComponent<LineRenderer>();
+        currentScene = SceneManager.GetActiveScene();
+        physicsScene = (currentScene == SceneManager.GetSceneByName("2DPhysics"));
+        Debug.Log("Starting");
+        lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.material.color = color;
-        
+        rb.isKinematic = true;
+        if (physicsScene)
+        {
+            radius = lineRenderer.startWidth * 0.5f;
+            _collider2D.enabled = false;
+        }
     }
 
     public void colourChange(Color Colour)
@@ -34,10 +45,20 @@ public class LineScript : MonoBehaviour
         return Vector2.Distance(lineRenderer.GetPosition(lineRenderer.positionCount - 1),pos) > RESOLUTION;
     }
 
-
+    //stop drawing and enable physics
     public void lineComplete()
     {
+        
         enabled = false;
+        if (physicsScene)
+        {
+            rb.isKinematic = false;
+            _collider2D.isTrigger |= false;
+            foreach (CircleCollider2D circle in GetComponents<CircleCollider2D>())
+            {
+                circle.enabled = true;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -45,15 +66,24 @@ public class LineScript : MonoBehaviour
     {
         if (_enabled)
         {
-            if (canDraw(transform.position  ))
+            newPos -= transform.position;
+            if (canDraw(newPos ))
             {
-                points.Add(transform.position);
+                points.Add(newPos);
 
                 lineRenderer.positionCount++;
-                lineRenderer.SetPosition(lineRenderer.positionCount - 1, transform.position);
+                lineRenderer.SetPosition(lineRenderer.positionCount - 1, newPos);
 
-                _collider2D.points = points.ToArray();
-                _collider2D.transform.localPosition = new Vector2(0 - transform.position.x, 0 - transform.position.y);
+                if (physicsScene)
+                {
+                    _collider2D.points = points.ToArray();
+
+                    //Add circle collider to allow interation
+                    CircleCollider2D circle = this.gameObject.AddComponent<CircleCollider2D>();
+                    circle.offset = newPos;
+                    circle.radius = radius;
+                    circle.enabled = false;
+                }
             }
         }
         
